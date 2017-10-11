3aba8e3de72083ea07c9b94e58b3f85e88a0fde3
huangfushan <243973775@qq.com>
2017-10-11 11:57:52 +0800

    huangfushan

--- webapp/js/villa_assess.js
+++ webapp/js/villa_assess.js
@@ -1,9 +1,10 @@
 $(function(){
-	$.post("getVillaComment",{},function(datas){
+	$.post("getComment",{"type":3},function(datas){//请求参数 ：type(1为别墅评论 2为驾校 3为军旅)
 		if (datas.status==0) {
-			var villa_assess_list = datas.data.villaComment;
+			var villa_assess_list = datas.data.Comment;
+			var html = "";
 			$.each(villa_assess_list,function(commentIndex,comment){
-				html+='<div class="assess-header"><div class="assess-header-icon"><img src="'+comment.headimageurl+' height="56px"></div><div class="assess-header-content"><div class="assess-header-star">'+comment.star+'</div><div class="assess-header-time">'+comment.userId+'</div><p class="assess-header-text">'+comment.content+'</p></div></div><div class="assess-pic"><img src="./images/alipay.png" height="56px"><img src="./images/circle.png"  height="56px"><img src="./images/circle.png"  height="56px"></div>'
+				html+='<div class="assess-header"><div class="assess-header-icon"><img src="'+comment.headimageurl+'" height="56px"></div><div class="assess-header-content"><div class="assess-header-star">'+comment.star+'</div><div class="assess-header-time">'+comment.commentTime+'</div><p class="assess-header-text">'+comment.content+'</p></div></div><div class="assess-pic"><img src="./images/alipay.png" height="56px"><img src="./images/circle.png"  height="56px"><img src="./images/circle.png"  height="56px"></div>'
 			});
 			$(".assessBox").html(html);
 		}else if (datas.status=="-20") {
