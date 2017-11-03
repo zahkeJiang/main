c10d4067e3312841ed9601cb072c76573f2df406
zahkeJiang <1345802063@qq.com>
2017-11-03 21:44:02 +0800

    debug queryOrder -Jiang

--- webapp/js/payResult.js
+++ webapp/js/payResult.js
@@ -11,18 +11,18 @@ $(function(){
     //发送请求判断支付完成与否
     $.post("queryOrder.action",{"ordernumber":orderNumber},function(obj){
         if(obj.status=="0"){
-            var order_number = obj.out_trade_no;
-            var ds_price = obj.price;
+            var order_number = obj.data.out_trade_no;
+            var ds_price = obj.data.price;
             $(".order_number").html(orderNumber);
             $(".ds_price").html(ds_price);
             $(".pay_success").show();
             $(".view_order p").click(function(){
-                window.location.href="orderInformation.html?ordernumber="+order_number;
+                window.location.href="orderInformation.html?ordernumber="+orderNumber;
             });
         }else if (obj.status=="-30") {
             $(".pay_fail").show();
             $(".view_order p").click(function(){
-                window.location.href="orderInformation.html?ordernumber="+order_number;
+                window.location.href="orderInformation.html?ordernumber="+orderNumber;
             });
         }
     },"json");
